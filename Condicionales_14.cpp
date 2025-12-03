fc4fbe32ca92f98239a0d5c228d3bd8cabdb3b3e
carlosdann727@gmail.com <carlosdann727@gmail.com>
2025-12-02 21:23:56 -0600

    Añadir condicional

--- Condicionales_14.cpp
+++ Condicionales_14.cpp
@@ -5,8 +5,38 @@ using namespace std;
 int main()
 {
 	
-	cout<<"Pruebas ignore";
-	cout<<"Terrible";
+	double x,y;
+	cout<<"Ingrese x y y en ese respectivo orden"<<endl;
+	cin>>x>>y;
+	
+	if(x == 0 and y == 0)
+	{
+		cout<<"El punto esta sobre el origen";
+	}
+	else if(y == 0 and x!=0)
+	{
+		cout<<"El punto esta sobre el eje x";
+	}
+	else if(y != 0 and x==0)
+	{
+		cout<<"El punto esta sobre el eje y";
+	}
+	else if(y > 0 and x >0)
+	{
+		cout<<"El punto esta en el primer cuadrante";
+	}
+	else if(y > 0 and x < 0)
+	{
+		cout<<"El punto esta en el segundo cuadrante";
+	}
+	else if(y < 0 and x < 0)
+	{
+		cout<<"El punto esta en el tercer cuadrante";
+	}
+	else 
+	{
+		cout<<"El punto esta en el cuarto cuadrante";
+	}
 	
 	
 	return 0;
