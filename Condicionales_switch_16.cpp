1c9abd4c52062f9f053b368240100641f9b7f02c
carlosdann727@gmail.com <carlosdann727@gmail.com>
2025-12-12 01:31:24 -0600

    commit 5

---
+++ Condicionales_switch_16.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	
+	
+	//Estructura switch 
+	// Es una estructura condicional que se divide en casos y es conveniente usarla cuando usted quiere realizar varios casos y en cada caso
+	// hacer algo distinto
+	
+	/*
+	Su sintaxis en c++ es la siguiente:
+	switch (n) donde n es el numero de caso o la letra en cuestion
+	{
+	case x :
+	accion
+	break;
+	
+	case y:
+	accion 
+	break;
+	
+	deafault:
+	accion 
+	break;
+	}	
+	*/
+	
+	//Ejercicio 1 :
+	// Programa que te diga el dia de la semana segun su numero siendo 1 el lunes 2 el martes etc y haga las respectivas validaciones
+	
+/*	int n;
+	cout<<"Ingresa un numero del 1 al 7: ";
+	cin>>n;
+	
+	switch (n)
+	{
+		case 1:
+			cout<<"Lunes";
+			break;
+		
+		case 2:
+			cout<<"Martes";
+			break;
+			
+		case 3:
+			cout<<"Miercoles";
+			break;
+		
+		case 4:
+			cout<<"Jueves";
+			break;
+		
+		case 5:
+			cout<<"Viernes";
+			break;
+		
+		case 6:
+			cout<<"Sabado";
+			break;
+			
+		case 7:
+			cout<<"Domingo";
+			break;
+		
+		default: 
+			cout<<"Debe ingresar un numero entre 1 y 7";
+			break;
+	}
+	*/
+	// Ejercicio 2
+	// Programa en el cual se haga una operacion basica como suma , resta ,division y multiplicacion dependiendo
+	// del numero que ingrese el usuario si ingresa 1 se hara la suma si ingresa 2 la resta y asi sucesivamente y hacer validaciones correspondientes
+	
+	
+	return 0;
+}
